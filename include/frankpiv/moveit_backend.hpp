42b7263fcd4e8f1f07789311b53a1b591ae78c4b
Martin Lelis <martin.lelis@nct-dresden.de>
2021-07-05 13:17:07 +0200

    remove moveit async motion

--- include/frankpiv/moveit_backend.hpp
+++ include/frankpiv/moveit_backend.hpp
@@ -8,16 +8,9 @@
 namespace frankpiv::backend {
     class MoveitBackend : public GeneralBackend {
     private:
-        static constexpr int SPINLOCK_WAIT = 1000;
         float eef_step;
         float jump_threshold;
-        bool async_motion;
         moveit::planning_interface::MoveGroupInterface *robot;
-        std::list<boost::thread::id> threads_list;
-        boost::mutex threads_list_lock;
-        boost::atomic<bool> terminating;
-
-        bool movePYRZInternal(const Eigen::Vector4d &pyrz, bool degrees);
 
     protected:
         void initialize() override;
@@ -29,13 +22,7 @@ namespace frankpiv::backend {
         bool moveRobotCartesian(const Eigen::Affine3d &target_pose) override;
 
     public:
-
-        int SHUTDOWN_TIMEOUT = 5000;
-
         explicit MoveitBackend(const YAML::Node &config, const std::string& node_name = "pivot_controller", bool async_motion = false);
-
-        bool movePYRZ(const Eigen::Vector4d &pyrz, bool degrees = false) override;
-
     };
 }
 
--- src/frankpiv/native/moveit_backend.cpp
+++ src/frankpiv/native/moveit_backend.cpp
@@ -11,27 +11,13 @@ namespace frankpiv::backend {
         this->eef_step = get_config_value<float>(moveit_config, "eef_step")[0];
         this->jump_threshold = get_config_value<float>(moveit_config, "jump_threshold")[0];
         this->robot = nullptr;
-        this->async_motion = async_motion;
-        this->threads_list = std::list<boost::thread::id>();
-        this->terminating = new boost::atomic<bool>();
     }
 
     void MoveitBackend::initialize() {
         this->robot = new moveit::planning_interface::MoveGroupInterface(this->getRobotName());
-        this->terminating = false;
     }
 
     void MoveitBackend::finish() {
-        this->terminating = true;
-        boost::posix_time::ptime start = boost::posix_time::second_clock::local_time();
-        while (!this->threads_list.empty()) {
-            boost::posix_time::time_duration duration = boost::posix_time::second_clock::local_time() - start;
-            if (duration.total_milliseconds() > this->SHUTDOWN_TIMEOUT) {
-                this->robot->stop();
-                break;
-            }
-            boost::this_thread::sleep(boost::posix_time::milliseconds(MoveitBackend::SPINLOCK_WAIT));
-        }
         this->robot = nullptr;
     }
 
@@ -40,49 +26,13 @@ namespace frankpiv::backend {
     }
 
     bool MoveitBackend::moveRobotCartesian(const Eigen::Affine3d &target_pose) {
-        bool move = true;
-        if (this->async_motion) {
-            boost::thread::id most_recent_thread = *this->threads_list.end();
-            move = boost::this_thread::get_id() == most_recent_thread;
+        std::vector<geometry_msgs::Pose> waypoints;
+        waypoints.push_back(to_pose_msg(target_pose));
+        moveit_msgs::RobotTrajectory trajectory;
+        double fraction = this->robot->computeCartesianPath(waypoints, this->eef_step, this->jump_threshold, trajectory);
+        if (fraction != 1.) {
+            return false;
         }
-        if (move) {
-            std::vector<geometry_msgs::Pose> waypoints;
-            waypoints.push_back(to_pose_msg(target_pose));
-            moveit_msgs::RobotTrajectory trajectory;
-            double fraction = this->robot->computeCartesianPath(waypoints, this->eef_step, this->jump_threshold, trajectory);
-            if (fraction != 1.) {
-                return false;
-            }
-            if (!this->terminating) {
-                return this->robot->execute(trajectory) == 1;
-            }
-        }
-        return false;
+        return this->robot->execute(trajectory) == 1;
     }
-
-    bool MoveitBackend::movePYRZInternal(const Eigen::Vector4d &pyrz, bool degrees) {
-        this->threads_list_lock.lock();
-        this->threads_list.push_back(boost::this_thread::get_id());
-        this->threads_list_lock.unlock();
-        this->robot->stop();
-        bool status = false;
-        if (!this->terminating) {
-            status = GeneralBackend::movePYRZ(pyrz, degrees);
-        }
-        this->threads_list_lock.lock();
-        this->threads_list.remove(boost::this_thread::get_id());
-        this->threads_list_lock.unlock();
-        return status;
-    }
-
-    bool MoveitBackend::movePYRZ(const Eigen::Vector4d &pyrz, bool degrees) {
-        if (this->async_motion) {
-            boost::thread(&MoveitBackend::movePYRZInternal, this, pyrz, degrees);
-            //TODO handle status flag of async motion
-            return true;
-        } else {
-            return this->movePYRZInternal(pyrz, degrees);
-        }
-    }
-
 }
